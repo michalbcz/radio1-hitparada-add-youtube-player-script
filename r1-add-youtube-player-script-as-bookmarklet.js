cc0b754561f9255fe5c5bda0e80c4875d4645e48
Michal Bernhard <michal@bernhard.cz>
2011-12-18 10:32:43 +0100

    http protocol on raw.github.com stops working - changed to https

--- r1-add-youtube-player-script-as-bookmarklet.js
+++ r1-add-youtube-player-script-as-bookmarklet.js
@@ -5,7 +5,7 @@
  * tak tento bookmarklet ma na startosti pouze naloadovani scriptu z externiho zdroje. 
  */
 var radioJednaScript = document.createElement("script");
-radioJednaScript.src = "http://raw.github.com/michalbcz/radio1-hitparada-add-youtube-player-script/master/r1-add-youtube-player-script.js";
+radioJednaScript.src = "https://raw.github.com/michalbcz/radio1-hitparada-add-youtube-player-script/master/r1-add-youtube-player-script.js";
 radioJednaScript.type = "text/javascript";
 document.getElementsByTagName("head")[0].appendChild(radioJednaScript);
 void(0); 
